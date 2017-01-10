24e896cc7fec3483ac4dd9b6bee13f76d44ffbf7
dennyo <bijl1507@student.nhl.nl>
2017-01-10 12:44:33 +0100

    movies/series fix

--- Parsers/Parser Movies-Series/movies/src/bigdata/Main.java
+++ Parsers/Parser Movies-Series/movies/src/bigdata/Main.java
@@ -13,6 +13,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.*;
 
+/**
+ * @author denny
+ * parses the movies.list file and separates the data into .csv files for movies and series
+ */
 
 public class Main {
 
@@ -22,15 +26,16 @@ public class Main {
 
     public static void BufferedStream() throws IOException {
 
-        Pattern rSeries = Pattern.compile("(?:\\\")([^\\\"]+)(?:\\\")(?:[\\s]*(?:\\())(\\d{4})(?:\\/)?([IVXCM]*)?(?:\\)?[\\s]*)(?:(?:\\{)+(?:(?:([^\\}]*)(?:\\()(?:#)?([^\\)]*)\\))*)(?:\\})+)?(?:[\\s]*)(\\{\\{[\\S]*\\}\\})?(?:[\\s]*)(\\d{4})?(?:.*)");
-        Pattern rMovies = Pattern.compile("([^\\\"]+)\\s(?:(?:\\())(\\d{4})(?:\\/)?([IVXCM]*)?(?:\\)?)(?:\\s*(?:\\()([TV]*)?(?:\\)))?(?:(?:[\\s]*)(\\{\\{[\\S]*\\}\\})?)(?:.*)?");
+        Pattern rSeries = Pattern.compile("(?:\\\")([^\\\"]+)(?:\\\")(?:[\\s]*(?:\\())(\\d{4}|\\?{4})(?:\\/)?([IVXCM]+)?(?:\\)?[\\s]*)(?:(?:\\{)+(?:(?:([^\\}]*)(?:\\()(?:#)?([^\\)]*)\\))*)(?:\\})+)?(?:[\\s]*)(\\{\\{[\\S]*\\}\\})?(?:[\\s]*)(\\d{4}|\\?{4})?(?:.*)");
+        Pattern rMovies = Pattern.compile("([^\\\"]+)\\s(?:(?:\\())(\\d{4}|\\?{4})(?:\\/)?([IVXCM]+)?(?:\\)?)(?:\\s*(?:\\()([TVG]+)?(?:\\)))?(?:(?:[\\s]*)(\\{\\{[\\S]*\\}\\})?)(?:.*)?");
 
-        Pattern rEpisodeDate = Pattern.compile("(\\d\\d\\d\\d)(\\-)(\\d\\d)(\\-)(\\d\\d)");
+        Pattern rEpisodeDate = Pattern.compile("(\\d{4})(\\-)(\\d{2})(\\-)(\\d{2})");
         Pattern rEpisodeSeason = Pattern.compile("([0-9]+)(?:\\.)([0-9]+)");
 
         BufferedWriter fws = new BufferedWriter(new FileWriter("series.csv"));
         BufferedWriter fwm = new BufferedWriter(new FileWriter("movies.csv"));
 
+        // Generate the headers and specify columns for csv files
         List<String> seriecolumns = Arrays.asList("Title", "SerieStarted", "Quarter", "EpisodeName", "EpisodeDate", "SeasonNr", "EpisodeNr", "EpisodeYear", "State");
         List<String> moviecolumns = Arrays.asList("Title", "Year", "Quarter", "Medium", "State");
         String listString = "";
@@ -53,6 +58,8 @@ public class Main {
         try(BufferedReader br = new BufferedReader(new FileReader("movies.list"))) {
             for(String line; (line = br.readLine()) != null; ) {
                 listString = "";
+
+                //Try to match the line to both regex to check whether it's a movie or series
                 Matcher ms = rSeries.matcher(line);
                 Matcher mm = rMovies.matcher(line);
 
@@ -60,18 +67,21 @@ public class Main {
                     List<String> row = new ArrayList<String>();
 
                     for(int i=1;i<4;i++){
-                        if ("".equals(ms.group(i))) row.add("null");
+                        if ("".equals(ms.group(i)) || "????".equals(ms.group(i))) row.add("null");
                         else row.add(ms.group(i));
                     }
 
                     if(ms.group(4)!= null)
                     {
                         if ("".equals(ms.group(4))) row.add("null");
-                        else row.add(ms.group(4));
+                        else row.add(ms.group(4).trim());
 
                         if(ms.group(5) != null){
-                            Matcher mDate = rEpisodeDate.matcher(ms.group(5).toString());
-                            Matcher mSeason = rEpisodeSeason.matcher(ms.group(5).toString());
+
+                            // Check whether the episode has a season and episode number or is specified by release date;
+                            // Make the other null
+                            Matcher mDate = rEpisodeDate.matcher(ms.group(5));
+                            Matcher mSeason = rEpisodeSeason.matcher(ms.group(5));
 
                             if(mDate.find()){
                                 row.add(mDate.group(0));
@@ -84,22 +94,26 @@ public class Main {
                                 row.add(mSeason.group(2));
                             }
                         }
+
+                        //if the episode has only a name add null for date, season and episode
                         else { for(int i=0;i<3;i++){row.add("null");} }
 
                         String state = "null";
                         if(ms.group(6)!=null){
                             state =  "Suspended";
                         }
-                        if("".equals(ms.group(7))) row.add("null");
+
+                        if("".equals(ms.group(7)) ||"????".equals(ms.group(7))) row.add("null");
                         else row.add(ms.group(7));
 
                         row.add(state);
                     }
 
                     else {
+                        // if group 4 is not found, it means it's not an episode but the series itself, or a suspended series
                         for(int i=4;i<7;i++)row.add("null");
 
-                        if("".equals(ms.group(7))) row.add("null");
+                        if("".equals(ms.group(7)) ||"????".equals(ms.group(7))) row.add("null");
                         else row.add(ms.group(7));
 
                         if(ms.group(6) != null) row.add("Suspended");
@@ -114,6 +128,10 @@ public class Main {
 
                     fws.write(listString + "\n");
                 }
+
+                // if there is no match for series try to find a match for movies;
+                // prevent that any incomplete/corrupt series entries get picked up by movies
+                // (all series name start with quotes at the beginning of the line, movies don't)
                 else if (mm.find() && !(line.startsWith("\""))) {
                     List<String> row = new ArrayList<String>();
 
