ad6f3eabbacf15453f58268c380fcb20d1a12b44
dennyo <bijl1507@student.nhl.nl>
2017-01-19 19:46:09 +0100

    sql queries quickfix 7.0

--- serieratings.sql
+++ serieratings.sql
@@ -10,7 +10,8 @@ episode_nr integer,
 episode_year integer,
 state text,
 rating_major integer,
-rating_minor integer
+rating_minor integer,
+voters integer
 );
 
 CREATE INDEX title_serie_idx ON final.serie(title);
@@ -21,19 +22,17 @@ season_nr, episode_nr, episode_year, state)
 SELECT title, serie_started, quarter, episode_name, episode_date,
 season_nr, episode_nr, episode_year, state
 FROM staging.series;
-
-UPDATE final.serie 
-SET title = CONCAT('"',CONCAT(title,'"'));
   
 UPDATE final.serie S
 SET rating_major = R.rating_major,
-rating_minor = R.rating_minor
+rating_minor = R.rating_minor,
+voters = R.voters
 FROM staging.rating_series R
-WHERE S.title = R.title AND
+WHERE CONCAT('"', CONCAT(S.title, '"')) = R.title AND
 (S.episode_name = R.episode_name OR
  (S.episode_name IS NULL AND R.episode_name IS NULL))
 	  AND (S.season_nr = R.season_nr OR
       (S.season_nr IS NULL AND R.season_nr IS NULL))
 	  AND (S.episode_nr = R.episode_nr OR
       (S.episode_nr IS NULL AND R.episode_nr IS NULL))
-AND R.rating_major IS NOT NULL AND R.rating_minor IS NOT NULL;
+AND R.rating_major IS NOT NULL AND R.rating_minor IS NOT NULL AND R.voters IS NOT NULL;
